9beb174bd91354772f5e49e00c2fd7ca02584bd9
Vasanth Subramanian <vasanth.subramanian@glossier.com>
2021-06-24 08:03:13 -0400

    Fixed typos/lint

--- update_users_with_store_credit.rb
+++ update_users_with_store_credit.rb
@@ -4,14 +4,16 @@ user_emails_country = %w[vasanth.subramanian@glossier.com:US]
 actor_email = 'vasanth.subramanian@glossier.com'
 # memo = ''
 actor = Spree::User.find_by(email: actor_email)
-reason = 'retroactive_promotion_technical_issue',
-         country_currency_hash = { 'US' => 'USD', 'SE' => 'SEK', 'GB' => 'GBP', 'CA' => 'CAD', 'IE' => 'EUR' }
-amount_to_credit_hash = { 'USD' => 5, 'SEK' => 50, 'GB' => 5, 'CAD' => 5, 'IE' => 5 }
+reason = 'retroactive_promotion_technical_issue'
+country_currency_hash = { 'US' => 'USD', 'SE' => 'SEK', 'GB' => 'GBP',
+                          'CA' => 'CAD', 'IE' => 'EUR' }
+amount_to_credit_hash = { 'USD' => 5, 'SEK' => 50, 'GB' => 5, 'CAD' => 5,
+                          'IE' => 5 }
 credit_category_name = 'Customer Service'
 credit_category = ::Spree::StoreCreditCategory.find_by!(name: credit_category_name)
 
 results = []
-user_emails.each do |obj|
+user_emails_country.each do |obj|
   obj_array = obj.split(':')
   email = obj_array[0]
   country = obj_array[1]
